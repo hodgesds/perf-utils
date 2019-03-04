dbfbdf00e2aa24db7a2a4ec8dea55b9672119949
Daniel Hodges <hodges.daniel.scott@gmail.com>
2019-03-03 21:35:08 -0500

    Add ProfileTracepoint for profilng kernel tracepoints

--- events.go
+++ events.go
@@ -1,6 +1,26 @@
 package perf
 
-import "strings"
+import (
+	"fmt"
+	"strconv"
+	"strings"
+	"unsafe"
+
+	"golang.org/x/sys/unix"
+)
+
+const (
+	// PERF_TYPE_TRACEPOINT is a kernel tracepoint.
+	PERF_TYPE_TRACEPOINT = 2
+)
+
+// TracepointEvent is a event that is defined in the kernel.
+type TracepointEvent struct {
+	Type   string
+	Name   string
+	Path   string
+	Config uint64
+}
 
 // AvailableEvents returns the list of available events.
 func AvailableEvents() (map[string][]string, error) {
@@ -35,4 +55,50 @@ func AvailableTracers() ([]string, error) {
 func CurrentTracer() (string, error) {
 	res, err := fileToStrings(TracingDir + "/current_tracer")
 	return res[0], err
+}
+
+// getTracepointConfig is used to get the configuration for a trace event.
+func getTracepointConfig(kind, event string) (uint64, error) {
+	res, err := fileToStrings(TracingDir + fmt.Sprintf("/events/%s/%s/id", kind, event))
+	if err != nil {
+		return 0, err
+	}
+	return strconv.ParseUint(res[0], 10, 64)
+}
+
+// ProfileTracepoint is used to profile a kernel tracepoint event. Events can
+// be listed with `perf list` for Tracepoint Events or in the
+// /sys/kernel/debug/tracing/events directory with the kind being the directory
+// and the event being the subdirectory.
+func ProfileTracepoint(kind, event string, pid, cpu int, opts ...int) (Profiler, error) {
+	config, err := getTracepointConfig(kind, event)
+	if err != nil {
+		return nil, err
+	}
+	eventAttr := &unix.PerfEventAttr{
+		Type:        PERF_TYPE_TRACEPOINT,
+		Config:      config,
+		Size:        uint32(unsafe.Sizeof(unix.PerfEventAttr{})),
+		Bits:        unix.PerfBitDisabled | unix.PerfBitExcludeHv,
+		Read_format: unix.PERF_FORMAT_TOTAL_TIME_RUNNING | unix.PERF_FORMAT_TOTAL_TIME_ENABLED,
+		Sample_type: PERF_SAMPLE_IDENTIFIER,
+	}
+	var eventOps int
+	if len(opts) > 0 {
+		eventOps = opts[0]
+	}
+	fd, err := unix.PerfEventOpen(
+		eventAttr,
+		pid,
+		cpu,
+		-1,
+		eventOps,
+	)
+	if err != nil {
+		return nil, err
+	}
+
+	return &profiler{
+		fd: fd,
+	}, nil
 }
