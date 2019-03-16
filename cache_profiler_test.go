a2821610107ac5b44834c88c17ba9bc59437d689
Daniel Hodges <hodges.daniel.scott@gmail.com>
2019-03-16 19:07:26 -0400

    Add test for CacheProfiler

--- cache_profiler_test.go
+++ cache_profiler_test.go
@@ -1,1 +1,30 @@
 package perf
+
+import (
+	"os"
+	"testing"
+)
+
+func TestCacheProfiler(t *testing.T) {
+	p := NewCacheProfiler(os.Getpid(), 0)
+	defer func() {
+		if err := p.Close(); err != nil {
+			t.Fatal(err)
+		}
+	}()
+
+	if err := p.Reset(); err != nil {
+		t.Fatal(err)
+	}
+	if err := p.Start(); err != nil {
+		t.Fatal(err)
+	}
+	_, err := p.Profile()
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if err := p.Stop(); err != nil {
+		t.Fatal(err)
+	}
+}
