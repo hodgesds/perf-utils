e539b9eb01c3446f9dcd228ea9611f0ef92230e3
Daniel Hodges <hodges.daniel.scott@gmail.com>
2020-11-23 19:47:28 -0500

    Add MSR helpers

--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module github.com/hodgesds/perf-utils
 go 1.14
 
 require (
+	github.com/stretchr/testify v1.3.0
 	go.uber.org/multierr v1.6.0
 	golang.org/x/sys v0.0.0-20190219203350-90b0e4468f99
 )
---
+++ msr_test.go
@@ -0,0 +1,27 @@
+package perf
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+func TestMSRPaths(t *testing.T) {
+	_, err := MSRPaths()
+	require.Nil(t, err)
+}
+
+func TestMSR(t *testing.T) {
+	msrs, err := MSRPaths()
+	require.Nil(t, err)
+
+	msr, err := NewMSR(msrs[0])
+	require.Nil(t, err)
+
+	// TODO: This may only work on certain architectures :(
+	_, err = msr.Read(0x00)
+	require.Nil(t, err)
+
+	require.Nil(t, msr.Close())
+
+}
